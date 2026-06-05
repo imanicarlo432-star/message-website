f5f075c48ab034b152248ad1c81a19ec6e5cc21e
imanicarlo432-star <imanicarlo432@gmail.com>
2026-06-06 05:18:52 +0800

    Add files via upload

---
+++ members.js
@@ -0,0 +1,222 @@
+window.LIT_MEMBERS = [
+  {
+    "name": "小蛙",
+    "slug": "xiaowa",
+    "closed": "./xiaowa-closed.png",
+    "open": "./xiaowa-open.png",
+    "seal": "./xiaowa-seal.png",
+    "paper": "./xiaowa-paper.png",
+    "sealY": 38,
+    "sealScale": 1
+  },
+  {
+    "name": "芋泥",
+    "slug": "yuni",
+    "closed": "./yuni-closed.png",
+    "open": "./yuni-open.png",
+    "seal": "./yuni-seal.png",
+    "paper": "./yuni-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "猜猜",
+    "slug": "caicai",
+    "closed": "./caicai-closed.png",
+    "open": "./caicai-open.png",
+    "seal": "./caicai-seal.png",
+    "paper": "./caicai-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "虾仁",
+    "slug": "xiaren",
+    "closed": "./xiaren-closed.png",
+    "open": "./xiaren-open.png",
+    "seal": "./xiaren-seal.png",
+    "paper": "./xiaren-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "Kay",
+    "slug": "kay",
+    "closed": "./kay-closed.png",
+    "open": "./kay-open.png",
+    "seal": "./kay-seal.png",
+    "paper": "./kay-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "小越",
+    "slug": "xiaoyue",
+    "closed": "./xiaoyue-closed.png",
+    "open": "./xiaoyue-open.png",
+    "seal": "./xiaoyue-seal.png",
+    "paper": "./xiaoyue-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "西米",
+    "slug": "ximi",
+    "closed": "./ximi-closed.png",
+    "open": "./ximi-open.png",
+    "seal": "./ximi-seal.png",
+    "paper": "./ximi-paper.png",
+    "sealY": 0,
+    "sealScale": 1.24
+  },
+  {
+    "name": "柿子",
+    "slug": "shizi",
+    "closed": "./shizi-closed.png",
+    "open": "./shizi-open.png",
+    "seal": "./shizi-seal.png",
+    "paper": "./shizi-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "一桶",
+    "slug": "yitong",
+    "closed": "./yitong-closed.png",
+    "open": "./yitong-open.png",
+    "seal": "./yitong-seal.png",
+    "paper": "./yitong-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "泡泡",
+    "slug": "paopao",
+    "closed": "./paopao-closed.png",
+    "open": "./paopao-open.png",
+    "seal": "./paopao-seal.png",
+    "paper": "./paopao-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "秋秋",
+    "slug": "qiuqiu",
+    "closed": "./qiuqiu-closed.png",
+    "open": "./qiuqiu-open.png",
+    "seal": "./qiuqiu-seal.png",
+    "paper": "./qiuqiu-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "舟舟",
+    "slug": "zhouzhou",
+    "closed": "./zhouzhou-closed.png",
+    "open": "./zhouzhou-open.png",
+    "seal": "./zhouzhou-seal.png",
+    "paper": "./zhouzhou-paper.png",
+    "sealY": 38,
+    "sealScale": 1
+  },
+  {
+    "name": "单单",
+    "slug": "dandan",
+    "closed": "./dandan-closed.png",
+    "open": "./dandan-open.png",
+    "seal": "./dandan-seal.png",
+    "paper": "./dandan-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "YY",
+    "slug": "yy",
+    "closed": "./yy-closed.png",
+    "open": "./yy-open.png",
+    "seal": "./yy-seal.png",
+    "paper": "./yy-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "栗子",
+    "slug": "lizi",
+    "closed": "./lizi-closed.png",
+    "open": "./lizi-open.png",
+    "seal": "./lizi-seal.png",
+    "paper": "./lizi-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "Menu",
+    "slug": "menu",
+    "closed": "./menu-closed.png",
+    "open": "./menu-open.png",
+    "seal": "./menu-seal.png",
+    "paper": "./menu-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "木木",
+    "slug": "mumu",
+    "closed": "./mumu-closed.png",
+    "open": "./mumu-open.png",
+    "seal": "./mumu-seal.png",
+    "paper": "./mumu-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "跳跳",
+    "slug": "tiaotiao",
+    "closed": "./tiaotiao-closed.png",
+    "open": "./tiaotiao-open.png",
+    "seal": "./tiaotiao-seal.png",
+    "paper": "./tiaotiao-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "佳宁",
+    "slug": "jianing",
+    "closed": "./jianing-closed.png",
+    "open": "./jianing-open.png",
+    "seal": "./jianing-seal.png",
+    "paper": "./jianing-paper.png",
+    "sealY": 0,
+    "sealScale": 1.24
+  },
+  {
+    "name": "Marie",
+    "slug": "marie",
+    "closed": "./marie-closed.png",
+    "open": "./marie-open.png",
+    "seal": "./marie-seal.png",
+    "paper": "./marie-paper.png",
+    "sealY": 0,
+    "sealScale": 1.24
+  },
+  {
+    "name": "拾壹",
+    "slug": "shiyi",
+    "closed": "./shiyi-closed.png",
+    "open": "./shiyi-open.png",
+    "seal": "./shiyi-seal.png",
+    "paper": "./shiyi-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  },
+  {
+    "name": "萱萱",
+    "slug": "xuanxuan",
+    "closed": "./xuanxuan-closed.png",
+    "open": "./xuanxuan-open.png",
+    "seal": "./xuanxuan-seal.png",
+    "paper": "./xuanxuan-paper.png",
+    "sealY": 0,
+    "sealScale": 1
+  }
+];
